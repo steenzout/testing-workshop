9131a72fdaef7b64ae86f064708d06739d1d1e60
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-14 14:51:09 -0600

    api/demo/unit/__tests__/utils.js: implemented tests.

--- api/demo/unit/__tests__/utils.js
+++ api/demo/unit/__tests__/utils.js
@@ -1,1 +1,17 @@
-test('todo', () => {})
+import {makeMeASandwich} from '../utils'
+
+test('returns null when sandwich is not a string', () => {
+    const req = {query: {sandwich: false}}
+    const result = makeMeASandwich(req)
+
+    expect(result).toBeNull()
+})
+
+test('returns null when sandwich is a string', () => {
+    const sandwich = 'grilled cheese'
+
+    const req = {query: {sandwich: sandwich}}
+    const result = makeMeASandwich(req)
+
+    expect(result).toBe(sandwich)
+})
