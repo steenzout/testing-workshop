2d59eedd1cf47add53a735771077397b377dbca3
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-17 09:44:05 -0600

    tests/integration/articles.test.js: implemented tests.

--- api/tests/integration/articles.test.js
+++ api/tests/integration/articles.test.js
@@ -15,47 +15,97 @@ import {generateArticleForClient} from '../../../other/generate/article'
 //   some of the logic for dealing with articles and users
 //   (because you'll need to perform basic CRUD and it's
 //   nice to avoid that cruft in your tests)
+import axios from 'axios'
+import faker from 'faker'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api'
+})
+
+let server
+
+beforeAll(async () => {
+    server = await startServer()
+})
+
+afterAll(done => {
+    server.close(done)
+    api.defaults.headers.common.authorization = ''
+})
 
 describe('unauthorized', () => {
-  test('get with limit', async () => {
-    // TODO
-  })
+    test('get with limit', async () => {
+        const limit = 5
+        const resp = await api.get(`articles?limit=${limit}`)
 
-  test('get with offset', async () => {
-    // TODO
-  })
+        expect(resp.data.articles).toHaveLength(limit)
+    })
+
+    test('get with offset', async () => {
+        // TODO
+        expect(false).toBe(true)
+    })
 })
 
 describe('authorized', () => {
-  // // TODO
-  // tip: you're going to need to create a new user
-  //   for these tests and set up the API client to
-  //   use that user's token. You can look at the
-  //   API client in the client application to see
-  //   how this is done: client/src/shared/agent.js
 
-  test('post a new article', async () => {
-    // TODO
-  })
+    beforeAll(async () => {
+        const user = await generateRandomUser()
+        const userAuth = await login(user.token)
 
-  test('update an article', async () => {
-    // TODO
-  })
+        // setup authentication
+        api.defaults.headers.common.authorization = `Token ${currentUser.token}`
+    })
 
-  test('delete an article', async () => {
-    // TODO
-  })
+    afterAll(async () => {
+        // TODO delete users
+        // remove authentication
+        api.defaults.headers.common.authorization = ''
+    })
+
+    // tip: you're going to need to create a new user
+    //   for these tests and set up the API client to
+    //   use that user's token. You can look at the
+    //   API client in the client application to see
+    //   how this is done: client/src/shared/agent.js
+
+    test('post a new article', async () => {
+        const article = generateRandomArticle()
+        const postArticle = await api.post('articles', {article})
+            .then(r => r.data.article)
+
+        const getArticle = await api.get(`articles/${postArticle.article}`)
+            .then(r => r.data.article)
+
+        expect(getArticle).toMatchObject(article)
+    })
+
+    test('update an article', async () => {
+        // TODO
+        expect(false).toBe(true)
+    })
+
+    test('delete an article', async () => {
+        // TODO
+        expect(false).toBe(true)
+    })
 })
 
 //////// Elaboration & Feedback /////////
 /*
-http://ws.kcd.im/?ws=Testing&e=API%20Integration&em=
-*/
+ http://ws.kcd.im/?ws=Testing&e=API%20Integration&em=
+ */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(true).toBe(submitted)
+    const submitted = false // change this when you've submitted!
+    expect(true).toBe(submitted)
 })
 ////////////////////////////////
+
+async function generateRandomUser(overrides) {
+    const password = faker.internet.password()
+    const userData = generateUserData(Object.assign({password}, overrides))
+    return Object.assign(userData, {password})
+}
 
 //////// EXTRA CREDIT ////////
 
